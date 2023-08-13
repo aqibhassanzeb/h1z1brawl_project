dab76cd1dbafa26bc06fa17ec743c1b996b06a82
aqibhassanzeb <aqibhassanzeb@gmail.com>
2023-08-13 09:44:47 +0500

    push request and fixed bug then run

---
+++ client/src/components/404/404.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+import './404.scss'
+
+class NotFound extends Component {
+
+  render() {
+    return (
+      <div className="NotFound">
+        <p>Page Not Found</p>
+      </div>
+    )
+  }
+
+}
+
+export default NotFound
